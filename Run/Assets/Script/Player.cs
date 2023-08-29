5e6166f52a937fe5703eadf27f0cc73b465b615e
harp515 <128121328+harp515@users.noreply.github.com>
2023-08-29 21:15:34 +0900

    UI
    hp bar, wall

--- Run/Assets/Script/Player.cs
+++ Run/Assets/Script/Player.cs
@@ -1,15 +1,25 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Script
 {
     public class Player : MonoBehaviour
     {
-        [SerializeField] float jumpForce = 600.0f, doublejumpForce = 700.0f;
+        [SerializeField] float jumpForce, doublejumpForce;
         private Rigidbody2D _rd;
+        public Slider hpSlider;
 
         private bool _doubleJumpState = false;
         private bool _isground = false;
+        
+        private bool getDamage = true;
+        private float currentTime = 0;
+
+        public float noDamage = 2.0f;
+        public float maxhp = 300.0f;
+        public float hp = 300.0f;
+        public float Damage = 30;
 
         private void Start()
         {
@@ -18,12 +28,15 @@ namespace Script
 
         private void Update()
         {
+            hpSlider.value = (float)hp / (float)maxhp;
             Jump();
+            GetDamage();
         }
 
         // ReSharper disable Unity.PerformanceAnalysis
         void Jump()
         {
+            
             if (_rd.velocity.y == 0)
             {
                 _isground = true;
@@ -39,7 +52,7 @@ namespace Script
             if (_isground && Input.GetButtonDown("Jump"))
                 JumpAddForce();
             else if (_doubleJumpState && Input.GetButtonDown("Jump"))
-            {
+            {   
                 _doubleJumpState = false;
                 _rd.AddForce(Vector2.up * doublejumpForce);
                 Debug.Log("더블 점프");
@@ -51,6 +64,25 @@ namespace Script
         {
             Debug.Log("점프");
             _rd.AddForce(Vector2.up * jumpForce);
+        }
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.CompareTag("Wall") && getDamage)
+            {
+                hp -= Damage;
+                getDamage = false;
+            }
+        }
+
+        void GetDamage()
+        {
+            if (!getDamage)
+                currentTime += Time.deltaTime;
+                if (currentTime > noDamage)
+                {
+                    getDamage = true;
+                    currentTime = 0;
+                }
         }
     }
 }
